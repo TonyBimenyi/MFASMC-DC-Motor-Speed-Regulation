40e3d202b5d154e4f491226a882fdc5dcbc41f40
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2024-07-10 11:35:10 +0800

    ndiko ndakora yd

--- Reference/trajectory.py
+++ Reference/trajectory.py
@@ -2,6 +2,9 @@ import numpy as np
 
 def generate_trajectory():
     yd = np.zeros(400)
-    yd = 50 * np.cos(200 * np.pi /400) + 50
+    # t = np.linspace(0, 4 * np.pi, 400)
+    # yd = np.sin(t)
+
+    
 
     return yd
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import numpy as np
 
-from Controllers.MFASM_controller import MFASM_controller
+# from Controllers.MFASM_controller import MFASM_controller
 from Reference.trajectory import generate_trajectory
-from Output.output import Output
+# from Output.output import Output
 from Estimator.estimator import initialize_phi
 from Plot.plotter import plot_results
+
+yd = generate_trajectory()
+
+# for k in range(400):
+
+
+plot_results(yd)
