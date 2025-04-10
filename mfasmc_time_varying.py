1b29e2eddfa9508f93e1f3d5cac960f254660bc9
TonyBimenyi <tonyblaisb@gmail.com>
2025-04-10 15:03:25 +0800

    change sms

--- mfasmc_time_varying.py
+++ mfasmc_time_varying.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 d =5
 rho = 0.3
 eta = 1
-lamda = 0.5
+lamda = 50
 mu = 0.5
 epsilon = 10**-5
 alpha = 1
 omega = 1
 sigma = 1
 tau =1
+T =2
 gamma1 = 0.15
 gamma2 = 0.15
 gamma3 = 0.45
@@ -63,12 +64,12 @@ s4 = np.zeros((L, 1))
 
 # Simulation loop
 for k in range(1, L-1):
-    if k == 1:
+    if k == 0:
         phi1[0] = 1
         phi2[0] = 1
         phi3[0] = 1
         phi4[0] = 1
-    elif k == 2:
+    elif k == 1:
         phi1[k] = phi1[k - 1] + eta * u1[k - 1] / (mu + u1[k - 1]**2) * (y1[k] - y1[k - 1] - phi1[k - 1] * u1[k - 1])
         phi2[k] = phi2[k - 1] + eta * u2[k - 1] / (mu + u2[k - 1]**2) * (y2[k] - y2[k - 1] - phi2[k - 1] * u2[k - 1])
         phi3[k] = phi3[k - 1] + eta * u3[k - 1] / (mu + u3[k - 1]**2) * (y3[k] - y3[k - 1] - phi3[k - 1] * u3[k - 1])
@@ -79,10 +80,10 @@ for k in range(1, L-1):
         phi3[k] = phi3[k - 1] + (eta * (u3[k - 1] - u3[k - 2]) / (mu + (abs(u3[k - 1] - u3[k - 2]))**2)) * (y3[k] - y3[k - 1] - phi3[k - 1] * (u3[k - 1] - u3[k - 2]))
         phi4[k] = phi4[k - 1] + (eta * (u4[k - 1] - u4[k - 2]) / (mu + (abs(u4[k - 1] - u4[k - 2]))**2)) * (y4[k] - y4[k - 1] - phi4[k - 1] * (u4[k - 1] - u4[k - 2]))
 
-    si1[k] = yd[k] - 2 * y1[k] + y4[k]
-    si2[k] = y1[k] - 2 * y2[k] + y3[k]
-    si3[k] = y2[k] + yd[k] - 2 * y3[k]
-    si4[k] = y1[k] + y3[k] - 2 * y4[k]
+    si1[k+1] = yd[k+1] - 2 * y1[k+1] + y4[k+1]
+    si2[k+1] = y1[k+1] - 2 * y2[k+1] + y3[k+1]
+    si3[k+1] = y2[k+1] + yd[k+1] - 2 * y3[k+1]
+    si4[k+1] = y1[k+1] + y3[k+1] - 2 * y4[k+1]
 
     s1[k] = alpha * si1[k+1] - si1[k]
     s2[k] = alpha * si2[k+1] - si2[k]
@@ -107,11 +108,11 @@ for k in range(1, L-1):
         sm3[0] = 0
         sm4[0] = 0
     else:
-        sm1[k] = sm1[k-1] + ((omega * phi1[k])/(sigma+abs(phi1[k])**2))*((alpha*(si1[k])-si1[k])/(alpha*(y4[k]+yd[k]))-y1[k]+tau*np.sign(s1[k]))
-        sm2[k] = sm2[k-1] + ((omega * phi2[k])/(sigma+abs(phi2[k])**2))*((alpha*(si2[k])-si1[k])/(alpha*(y1[k]+y3[k]))-y2[k]+tau*np.sign(s2[k]))
-        sm3[k] = sm3[k-1] + ((omega * phi3[k])/(sigma+abs(phi3[k])**2))*((alpha*(si3[k])-si3[k])/(alpha*(y2[k]+yd[k]))-y3[k]+tau*np.sign(s3[k]))
-        sm4[k] = sm4[k-1] + ((omega * phi4[k])/(sigma+abs(phi4[k])**2))*((alpha*(si4[k])-si4[k])/(alpha*(y1[k]+y3[k]))-y4[k]+tau*np.sign(s4[k]))
-
+        sm1[k] = sm1[k-1] + (omega * phi1[k]/sigma+abs(phi1[k])**2 *alpha*si1[k]-si1[k]/alpha*y4[k]+yd[k]-y1[k]+tau*np.sign(s1[k]))
+        sm2[k] = sm2[k-1] + (omega * phi2[k]/sigma+abs(phi2[k])**2 *alpha*si2[k]-si2[k]/alpha*y4[k]+yd[k]-y1[k]+tau*np.sign(s2[k]))
+        sm1[k] = sm1[k-1] + (omega * phi1[k]/sigma+abs(phi1[k])**2 *alpha*si1[k]-si1[k]/alpha*y4[k]+yd[k]-y1[k]+tau*np.sign(s1[k]))
+        sm1[k] = sm1[k-1] + (omega * phi1[k]/sigma+abs(phi1[k])**2 *alpha*si1[k]-si1[k]/alpha*y4[k]+yd[k]-y1[k]+tau*np.sign(s1[k]))
+        
     if k == 1:
         u1[0] = 0.1
         u2[0] = 0.1
@@ -123,10 +124,10 @@ for k in range(1, L-1):
         u3[k] = mfa3[k] + gamma3 * sm3[k]
         u4[k] = mfa4[k] + gamma4 * sm4[k]
 
-    y1[0] = 1
-    y2[0] = 1
-    y3[0] = 1
-    y4[0] = 1
+    y1[0] = 0.1
+    y2[0] = 0.1
+    y3[0] = 0.1
+    y4[0] = 0.1
     y1[k + 1] = m / (rT * 0.1) * u1[k]
     y2[k + 1] = m / (rT * 0.1) * u2[k]
     y3[k + 1] = m / (rT * 0.3) * u3[k]
@@ -136,6 +137,9 @@ plt.figure(figsize=(10, 6))  # Adjust figure size for better visibility
 
 plt.plot(yd[:-1], '-y', linewidth=2.5, label=r'$y_d$')  
 plt.plot(y1[:-1], '--r', linewidth=2.5, label=r'$y_1$')
+plt.plot(y2[:-1], '-.b', linewidth=2.5, label=r'$y_2$')
+plt.plot(y3[:-1], '--k', linewidth=2.5, label=r'$y_3$')
+plt.plot(y4[:-1], '-.g', linewidth=2.5, label=r'$y_4$')
 
 # Enlarge font size for axis labels and legend
 plt.xlabel('Time step', fontsize=14)
