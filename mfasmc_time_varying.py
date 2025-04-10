073319076b4165def74a089ed7c8f0daf310079a
TonyBimenyi <tonyblaisb@gmail.com>
2025-04-10 09:57:58 +0800

    define arrays

--- mfasmc_time_varying.py
+++ mfasmc_time_varying.py
@@ -22,6 +22,37 @@ yd = np.zeros(L + 1)
 for k in range(L):
     yd[k] = 0.5 * np.sin(0.07 * np.pi * k) + 0.7 * np.cos(0.04 * np.pi * k)
 
+# Initialize arrays
+phi1 = np.zeros((L, 1))
+phi2 = np.zeros((L, 1))
+phi3 = np.zeros((L, 1))
+phi4 = np.zeros((L, 1))
+
+mfa1 = np.zeros((L, 1))
+mfa2 = np.zeros((L, 1))
+mfa3 = np.zeros((L, 1))
+mfa4 = np.zeros((L, 1))
+
+sm1 = np.zeros((L, 1))
+sm2 = np.zeros((L, 1))
+sm3 = np.zeros((L, 1))
+sm4 = np.zeros((L, 1))
+
+u1 = np.zeros((L, 1))
+u2 = np.zeros((L, 1))
+u3 = np.zeros((L, 1))
+u4 = np.zeros((L, 1))
+
+y1 = np.zeros((L + 1, 1))
+y2 = np.zeros((L + 1, 1))
+y3 = np.zeros((L + 1, 1))
+y4 = np.zeros((L + 1, 1))
+
+si1 = np.zeros((L, 1))
+si2 = np.zeros((L, 1))
+si3 = np.zeros((L, 1))
+si4 = np.zeros((L, 1))
+
 
 plt.figure(figsize=(10, 6))  # Adjust figure size for better visibility
 
